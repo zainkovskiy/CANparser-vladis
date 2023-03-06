cfc695dd6e8e761b6686b56f1ef8a0636098581d
Anton Zainkovskiy <Piton1808@gmail.com>
2023-03-06 20:51:14 +0100

    add function figeureOutParsing

--- src/index.js
+++ src/index.js
@@ -3,17 +3,29 @@ const request = require('request');
 const express = require('express');
 const convert = require('xml-js');
 
+const fugureOutParsing = (parser) => {
+  if (parser?.feed?.object?.length > 0) {
+    return parser?.feed?.object.map((item) => {
+      return {
+        id: item?.ExternalId?._text || '',
+        description: item?.Description?._text || ''
+      }
+    })
+  }
+  return []
+}
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  request('https://www.w3schools.com/xml/cd_catalog.xml', (err, response, body) => {
+  request('https://novosibirsk.vladis.ru/export_cian_1mtc2bvnyhv', (err, response, body) => {
     if (err) { res.send(err) }
     if (convert.xml2json(body, { compact: true, spaces: 4 })) {
       res.send({
         status: response.statusMessage,
-        data: convert.xml2json(body, { compact: true, spaces: 4 })
+        data: fugureOutParsing(JSON.parse(convert.xml2json(body, { compact: true, spaces: 2 }))),
       });
     } else {
       res.send({
